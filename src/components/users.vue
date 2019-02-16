0fa763d07e09c8ef399a4f7f0186224d6d8c830b
sh <1696203601@qq.com>
2019-02-16 19:01:38 +0800

    users

---
+++ src/components/users.vue
@@ -0,0 +1,197 @@
+<template>
+  <div>
+    <el-card class="box">
+      <!-- 面包屑 -->
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>首页</el-breadcrumb-item>
+        <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+        <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+      </el-breadcrumb>
+      <!-- 搜索 -->
+      <el-row class="searchBox">
+        <el-col>
+          <el-input
+            @clear="getAllUsers()"
+            clearable
+            placeholder="请输入内容"
+            v-model="query"
+            class="searchInput"
+          >
+            <el-button @click="searchUser()" slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+          <el-button 
+          @click ='showDiaAddUse()' type="primary">添加用户</el-button>
+        </el-col>
+      </el-row>
+      <!-- 表格 -->
+      <el-table :data="list" style="width: 100%">
+        <el-table-column prop="id" label="#" width="80"></el-table-column>
+        <el-table-column prop="username" label="姓名" width="100"></el-table-column>
+        <el-table-column prop="email" label="邮箱" width="140"></el-table-column>
+        <el-table-column prop="mobile" label="电话" width="140"></el-table-column>
+
+        <el-table-column label="创建日期" width="200">
+          <template slot-scope="scope">{{scope.row.create_time|fmtdate}}</template>
+        </el-table-column>
+        <el-table-column prop="address" label="用户状态" width="120">
+          <template slot-scope="scope">
+            <div>
+              <el-switch
+                v-model="scope.row.mg_state"
+                active-color="#13ce66"
+                inactive-color="#ff4949"
+              ></el-switch>
+            </div>
+          </template>
+        </el-table-column>
+
+        <el-table-column prop="address" label="操作" width="200">
+          <template slot-scope="scope">
+            <el-button
+            @click ='showDiaEditUse' 
+            type="primary" icon="el-icon-edit" circle size="mini" plain></el-button>
+            <el-button type="success" icon="el-icon-check" circle size="mini" plain></el-button>
+            <el-button 
+            @click ='showMsgBox(scope.row)' type="danger" icon="el-icon-delete" circle size="mini" plain></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="pagenum"
+        :page-sizes="[2, 4, 6, 8]"
+        :page-size="2"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="10"
+      ></el-pagination>
+    </el-card>
+     <el-dialog title="收货地址" :visible.sync="dialogFormVisibleAdd">
+      <el-form label-position="left" label-width="80px" :model="formdata">
+        <el-form-item label="姓名">
+          <el-input v-model="formdata.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="formdata.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱">
+          <el-input v-model="formdata.email"></el-input>
+        </el-form-item>
+        <el-form-item label="电脑">
+          <el-input v-model="formdata.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibleAdd = false">取 消</el-button>
+        <el-button type="primary" @click="addUser()">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      query: "",
+      pagenum: 1,
+      pagesize: 2,
+      total:-1,
+      list: [],
+      dialogFormVisibleAdd:false,
+      formdata:{
+          username:'',	
+          password:'',	
+          email:'',	
+          mobile:''
+      }
+    };
+  },
+  created() {
+    this.getTableData();
+   
+  },
+  methods: {
+      showMsgBox(user){
+          console.log(user);
+          
+          this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(async () => {
+            const res =await this.$http.delete(`users/:id`)
+            
+            const {meta:{msg,status}} = res.data;
+            if(status===200){
+            this.$message.success(msg)
+            this.getTableData();
+            }
+        }).catch(() => {
+        //  
+         this.$message.info('取消删除')        
+        });
+      },
+    async addUser(){
+        this.dialogFormVisibleAdd = {};
+         const res = await this.$http.post(`users`,this.formdata);
+         console.log(res);
+         this.dialogFormVisibleAdd = false;
+      },
+      showDiaAddUse(){
+         this.dialogFormVisibleAdd = true;
+      },
+      getAllUsers(){
+          this.getTableData();
+      },
+      searchUser(){
+          this.pagenum = '1';
+          this.getTableData();
+      },
+       handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+        this.pagenum = 1;
+       this.pagesize = val;
+       this.getTableData();
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
+         this.pagenum = val;
+        this.getTableData();
+      },
+    async getTableData() {
+      
+      const AUTH_TOKEN = localStorage.getItem("token");
+      console.log(AUTH_TOKEN);
+
+      this.$http.defaults.headers.common["Authorization"] = AUTH_TOKEN;
+      const res = await this.$http.get(
+        `users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${
+          this.pagesize
+        }`
+      );
+      console.log(res);
+
+      const {
+        data,
+        meta: { status, msg }
+      } = res.data;
+      if (status === 200) {
+        this.list = data.users;
+        this.total = data.total;
+        console.log(this.list);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.searchBox {
+  margin-top: 20px;
+}
+.searchInput {
+  width: 400px;
+}
+</style>
